12267ccf2273a44585d96ea871d10dd32f566074
ggu1012 <ggu1012@naver.com>
2021-11-25 02:15:21 +0900

    final test

--- dc/rtl/lxp32_sram.vhd
+++ dc/rtl/lxp32_sram.vhd
@@ -50,7 +50,6 @@ architecture rtl of lxp32_ram128x32 is
 begin
     G1: SRAM2RW128x32 
         port map(
-            asdfasldhfj
             CE1 => CE1,
             CE2 => CE2,
             WEB1 =>WEB1,
